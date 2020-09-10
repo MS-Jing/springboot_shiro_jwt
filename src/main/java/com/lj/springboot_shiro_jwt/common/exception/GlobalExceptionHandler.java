0006cea334c3546a7356cb34866730839a3e5a06
LuoJing <1126184155@qq.com>
2020-09-10 21:56:34 +0800

    修改了全局异常，断言无法捕获的问题

--- src/main/java/com/lj/springboot_shiro_jwt/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lj/springboot_shiro_jwt/common/exception/GlobalExceptionHandler.java
@@ -22,59 +22,31 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = ShiroException.class)
-    public Result handler(ShiroException e) {
-        log.error("Shiro异常：=============");
-        return Result.fail(401, e.getMessage(), null);
+    public Result handler(ShiroException e){
+        log.error("Shiro异常：============={}");
+        return Result.fail(401,e.getMessage(),null);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public Result handler(MethodArgumentNotValidException e) {
-        log.error("实体校验异常：=============");
+    public Result handler(MethodArgumentNotValidException e){
+        log.error("实体校验异常：============={}");
         BindingResult bindingResult = e.getBindingResult();
         ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
         return Result.fail(objectError.getDefaultMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = IllegalAccessError.class)
-    public Result handler(IllegalAccessError e) {
-        log.error("Assert异常：=============");
-        return Result.fail(e.getMessage());
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = JWTDecodeException.class)
-    public Result handler(JWTDecodeException e) {
-        log.error("JWTDecode异常：=============");
-        return Result.fail(e.getMessage());
-    }
-
-    //    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = SignatureVerificationException.class)
-    public Result handler(SignatureVerificationException e) {
-        log.error("SignatureVerification异常：=============");
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public Result handler(IllegalArgumentException e){
+        log.error("Assert异常：============={}");
         return Result.fail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = RuntimeException.class)
-    public Result handler(RuntimeException e) {
-        log.error("Runtime异常：=============");
-        return Result.fail(e.getMessage());
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = IllegalArgumentException.class)
-    public Result handler(IllegalArgumentException e) {
-        log.error("IllegalArgument异常：=============");
-        return Result.fail(e.getMessage());
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = Exception.class)
-    public Result handler(Exception e) {
-        log.error("异常：============={}");
+    public Result handler(RuntimeException e){
+        log.error("运行时异常：============={}");
         return Result.fail(e.getMessage());
     }
 
